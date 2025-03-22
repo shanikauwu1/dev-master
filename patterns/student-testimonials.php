150261930fbcec0f2577aaf54ab6b63ffcea3b8e
Arash Laali <arashlaali@Arashs-MacBook-Pro.local>
2025-03-22 09:45:36 +0330

    new pattern file

---
+++ patterns/student-testimonials.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Title: Student Testimonial
+ * Slug:dev-master/student-testimonial
+ * Categories:testimonials
+
+ */
+?>
+
+<!-- wp:media-text {"mediaId":89,"mediaLink":"https://www.shanikacode.in/school-site/?attachment_id=89","mediaType":"image","style":{"border":{"width":"0px","style":"none","radius":"0px"},"spacing":{"margin":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80","left":"var:preset|spacing|30","right":"var:preset|spacing|30"},"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}}} -->
+<div class="wp-block-media-text is-stacked-on-mobile" style="border-style:none;border-width:0px;border-radius:0px;margin-top:var(--wp--preset--spacing--80);margin-right:var(--wp--preset--spacing--30);margin-bottom:var(--wp--preset--spacing--80);margin-left:var(--wp--preset--spacing--30);padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40)">
+    <figure class="wp-block-media-text__media"><img src="https://www.shanikacode.in/school-site/wp-content/uploads/2025/03/pexels-george-dolgikh-551816-1326946-1024x683.jpg" alt="" class="wp-image-89 size-full" /></figure>
+    <div class="wp-block-media-text__content"><!-- wp:heading {"textAlign":"center","fontSize":"x-large"} -->
+        <h2 class="wp-block-heading has-text-align-center has-x-large-font-size">Student Testimonial</h2>
+        <!-- /wp:heading -->
+
+        <!-- wp:paragraph -->
+        <p>Testimonial: </p>
+        <!-- /wp:paragraph -->
+    </div>
+</div>
+<!-- /wp:media-text -->
